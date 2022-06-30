d2ff1f5eb7dd365decfefa6a1ea61a0666ff11b6
nkokina <mae9077@gmail.com>
2022-06-30 18:23:20 +0300

    Added project with fixes SearchTests.java

--- java/SingUpTests.java
+++ java/SingUpTests.java
@@ -8,13 +8,31 @@ import org.testng.annotations.Test;
 
 public class SingUpTests {
 
-    @Test
-    public static void whenZipCodeEnterCorrectThanFirstNameDisplayed() throws InterruptedException {
+    private WebDriver driver;
 
+
+    @BeforeClass
+    public void driver() {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @BeforeMethod
+    public void openSharelane() {
         driver.get("https://www.sharelane.com/cgi-bin/register.py");
+    }
+
+    @AfterClass
+    public void tearDown() {
+
+        driver.quit();
+    }
+
+    @Test
+    public static void whenZipCodeEnterCorrectThanFirstNameDisplayed() {
+
         driver.findElement(By.name("zip_code")).sendKeys("12345");
         WebElement continueButton = driver.findElement(By.cssSelector("[value='Continue']"));
         continueButton.click();
@@ -22,16 +40,10 @@ public class SingUpTests {
                 "Zip code should not be displayed");
         Assert.assertTrue(driver.findElement(By.name("first_name")).isDisplayed(),
                 "First Name input should be displayed");
-        Thread.sleep(1000);
-        driver.quit();
     }
 
     @Test
-    public void whenZipCodeEnterNotCorrectThanErrorMessageDisplayed() throws InterruptedException {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+    public void whenZipCodeEnterNotCorrectThanErrorMessageDisplayed() {
         WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
         zipCodeInput.sendKeys("7788954A");
         WebElement continueButton = driver.findElement(By.cssSelector("[value='Continue']"));
@@ -44,34 +56,21 @@ public class SingUpTests {
 
         Assert.assertTrue(driver.findElement(By.name("zip_code")).isDisplayed(),
                 "Zip code should be displayed");
-        Thread.sleep(1000);
-        driver.quit();
-
 
     }
 
     @Test
-    public void whenZipCodeNotEnterThanErrorMessageDisplayed() throws InterruptedException {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+    public void whenZipCodeNotEnterThanErrorMessageDisplayed()  {
         driver.findElement(By.name("zip_code")).sendKeys("");
         WebElement continueButton = driver.findElement(By.cssSelector("[value='Continue']"));
         continueButton.click();
         WebElement errorMassage = driver.findElement(By.className("error_message"));
         Assert.assertTrue(driver.findElement(By.name("zip_code")).isDisplayed(), "Zip code should be displayed");
         Assert.assertTrue(errorMassage.isDisplayed(), "An error message not appeared");
-        Thread.sleep(1000);
-        driver.quit();
     }
 
     @Test
-    public void whenEmptyFieldsInTheAuthorizationThenTheInscriptionAppearsTests() throws InterruptedException {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+    public void whenEmptyFieldsInTheAuthorizationThenTheInscriptionAppearsTests() {
         driver.findElement(By.name("zip_code")).sendKeys("12345");
         WebElement continueButton = driver.findElement(By.cssSelector("[value='Continue']"));
         continueButton.click();
@@ -84,16 +83,10 @@ public class SingUpTests {
         registerButton.click();
         WebElement errorMessage = driver.findElement(By.className("error_message"));
         Assert.assertTrue(errorMessage.isDisplayed(), "Does not display a message about invalid registration.");
-        Thread.sleep(1000);
-        driver.quit();
     }
 
     @Test
-    public void whenEnterCorrectDataInSingUpThanSuccessMessageDisplayTests() throws InterruptedException {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+    public void whenEnterCorrectDataInSingUpThanSuccessMessageDisplayTests()  {
         driver.findElement(By.name("zip_code")).sendKeys("12345");
         WebElement continueButton = driver.findElement(By.cssSelector("[value='Continue']"));
         continueButton.click();
@@ -107,16 +100,10 @@ public class SingUpTests {
         WebElement successMessage = driver.findElement(By.className("confirmation_message"));
         Assert.assertTrue(successMessage.isDisplayed(),
                 "Error in registration, does not display a message about successful registration.");
-        Thread.sleep(1000);
-        driver.quit();
     }
 
     @Test
-    public void whenYouEnteredNumbersInTheNameDuringRegistrationThenAnErrorMessageIsDisplayedTests() throws InterruptedException {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+    public void whenYouEnteredNumbersInTheNameDuringRegistrationThenAnErrorMessageIsDisplayedTests() {
         driver.findElement(By.name("zip_code")).sendKeys("12345");
         WebElement continueButton = driver.findElement(By.cssSelector("[value='Continue']"));
         continueButton.click();
@@ -130,7 +117,6 @@ public class SingUpTests {
         WebElement errorMessage = driver.findElement(By.className("error_message"));
         Assert.assertTrue(errorMessage.isDisplayed(),
                 "Does not display a message about invalid registration.");
-        Thread.sleep(1000);
-        driver.quit();
+
     }
 }
